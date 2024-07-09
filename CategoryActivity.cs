be1ad0c07924e76c13ae6cc3c9032895fd8d1a8d
Michaella Villamante <98160468+mvillamante@users.noreply.github.com>
2024-07-09 20:23:32 +0800

    Add files via upload

---
+++ CategoryActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gamepedia
+{
+    [Activity(Label = "Next Activity")]
+    public class CategoryActivity:AppCompatActivity
+    {
+        ImageButton returnBtn, ctg1, ctg2, ctg3;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_category);
+
+            returnBtn = FindViewById<ImageButton>(Resource.Id.returnBtn);
+            ctg1 = FindViewById<ImageButton>(Resource.Id.ctg1);
+            ctg2 = FindViewById<ImageButton>(Resource.Id.ctg2);
+            ctg3 = FindViewById<ImageButton>(Resource.Id.ctg3);
+
+            returnBtn.Click += ReturnBtn_Click;
+        }
+
+        private void ReturnBtn_Click(object sender, EventArgs e)
+        {
+            Finish();
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Android.Content;
+using System;
+
+namespace Gamepedia
+{
+    [Activity(Label = "Gamepedia", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ImageButton playBtn, helpBtn;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            playBtn = FindViewById<ImageButton>(Resource.Id.playBtn);
+            helpBtn = FindViewById<ImageButton>(Resource.Id.helpBtn);
+
+            playBtn.Click += PlayBtn_Click;
+            helpBtn.Click += HelpBtn_Click;
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(CategoryActivity));
+            StartActivity(intent);
+        }
+
+        private void HelpBtn_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(CategoryActivity));
+            StartActivity(intent);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
